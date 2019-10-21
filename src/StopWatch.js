a772d5b399b79ab190dcc00daab14aace5b26c4d
Daniel Ahl <daniel.ahl@studentmedia.se>
2019-10-21 13:18:09 +0200

    Fixed so that the startbutton says resume instead of start if clock has been running

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -19,21 +19,18 @@ class StopWatch extends React.Component {
     this.splitClock = this.splitClock.bind(this);
   }
 
-  componentDidMount() {
-    if(this.state.isRunning) {
-      const startTime = Date.now() - this.state.time;
-      this.timer = setInterval(() => {
-        this.setState({ time: Date.now() - startTime });
-      })
-    }
+  componentWillUnmount() {
+    clearInterval(this.timer);
   }
 
+  //  Formats the milliseconds to be used to show time.
   timeFormat(time) {
     let min = Math.floor((time / (1000 * 60)) % 60);
     let sec = ((time % 60000)/ 1000).toFixed(2);
     return `${min}:${sec}`;
   }
 
+  // Controllermethods
   startClock() {
     this.setState(state => {
         const startTime = Date.now() - this.state.time;
@@ -56,16 +53,18 @@ class StopWatch extends React.Component {
     this.setState({splits:splits});
   }
 
+
   render() {
-    const {splitClock, startClock, stopClock, resetClock} = this;
-    const {showTime, splits, isRunning} = this.state;
+    const {splitClock, startClock, stopClock, resetClock} = this; // Getting the methods from this.
+    const {showTime, time, splits, isRunning} = this.state; // Getting the properties of state.
+    
     return (
       <main>
         <h1>StopWatch</h1>
         <Clockface time={showTime} />
 
         <div className="btnBox">
-          <Controller name={isRunning ? 'stop' : 'start'} onClick={isRunning ? stopClock : startClock} class={isRunning ? 'red' : 'green'}/>
+          <Controller name={isRunning ? 'stop' : (time === 0 ? 'start':'resume')} onClick={isRunning ? stopClock : startClock} class={isRunning ? 'red' : 'green'}/>
           <Controller name={isRunning ? 'split' : 'reset'} onClick={isRunning ? splitClock : resetClock} class={isRunning ? 'green':'red'}/>
         </div>
         
