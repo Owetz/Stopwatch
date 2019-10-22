82891dca9a6b58f09fa31bcd5735a37fa46e898b
Daniel Ahl <daniel.ahl@studentmedia.se>
2019-10-22 10:45:48 +0200

    Updates

---
+++ precache-manifest.b8da639cc3a3df292050522c96e98e76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d32e37a9f78ec41e3fa07609934440c",
+    "url": "/Stopwatch/index.html"
+  },
+  {
+    "revision": "3d5ee04a98b537642c71",
+    "url": "/Stopwatch/static/css/main.86e2e414.chunk.css"
+  },
+  {
+    "revision": "859378a00ef53d73e94e",
+    "url": "/Stopwatch/static/js/2.3e4eda96.chunk.js"
+  },
+  {
+    "revision": "3d5ee04a98b537642c71",
+    "url": "/Stopwatch/static/js/main.a9fbfbfb.chunk.js"
+  },
+  {
+    "revision": "6e6988f1c98a3a92db5e",
+    "url": "/Stopwatch/static/js/runtime-main.0482b81d.js"
+  }
+]);
